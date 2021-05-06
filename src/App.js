3c1193926663baef26e6a0b36531bbd842a397d2
FabioReact <fabio.react.dev@gmail.com>
2021-05-06 16:59:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import LearnerDetails from './components/LearnerDetails'
 
 function App() {
+  let [selectedUser, setSelectedUser] = useState(null)
+
+  const learners = [
+    {
+      id: 1,
+      firstname: "Laurent",
+      lastname: "Mazouin",
+      email: "laurent.mazouin@email.com",
+      score: 8,
+    },
+    {
+      id: 2,
+      firstname: "Godefroy",
+      lastname: "Diakite",
+      email: "godefroy.diakite@email.com",
+      score: 10,
+    },
+    {
+      id: 3,
+      firstname: "Alizée",
+      lastname: "Vasseur",
+      email: "alizee.vasseur@email.com",
+      score: 7,
+    },
+  ]
+
+  const averageScore = (learners.reduce((accumulator, learner) => {
+    return accumulator + learner.score
+  }, 0) / learners.length).toPrecision(3)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>List of learners</h1>
+      <p>Average learner score: {averageScore}</p>
+      <ul>
+        {
+          learners.map(learner => 
+            <li
+              onClick={() => {setSelectedUser(learner.id)}}
+              key={learner.id}
+            >
+              <span>{learner.firstname}</span>
+              {selectedUser === learner.id && <LearnerDetails learner={learner} />}
+            </li>
+          )
+        }
+      </ul>
+    </main>
   );
 }
 
