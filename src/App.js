a32ab696686afa2cc4e8b14367fdc7be979defd7
FabioReact <fabio.react.dev@gmail.com>
2021-05-07 14:34:33 +0200

    add reactfire, react-router-dom, add learner form

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
-import { useState } from 'react'
-import LearnerDetails from './components/LearnerDetails'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './pages/Home'
+import AddLearner from './pages/AddLearner'
 
 function App() {
-  let [selectedUser, setSelectedUser] = useState(null)
-
-  const learners = [
-    {
-      id: 1,
-      firstname: "Laurent",
-      lastname: "Mazouin",
-      email: "laurent.mazouin@email.com",
-      score: 8,
-    },
-    {
-      id: 2,
-      firstname: "Godefroy",
-      lastname: "Diakite",
-      email: "godefroy.diakite@email.com",
-      score: 10,
-    },
-    {
-      id: 3,
-      firstname: "Alizée",
-      lastname: "Vasseur",
-      email: "alizee.vasseur@email.com",
-      score: 7,
-    },
-  ]
-
-  const averageScore = (learners.reduce((accumulator, learner) => {
-    return accumulator + learner.score
-  }, 0) / learners.length).toPrecision(3)
-
   return (
-    <main>
-      <h1>List of learners</h1>
-      <p>Average learner score: {averageScore}</p>
-      <ul>
-        {
-          learners.map(learner => 
-            <li
-              onClick={() => {setSelectedUser(learner.id)}}
-              key={learner.id}
-            >
-              <span>{learner.firstname}</span>
-              {selectedUser === learner.id && <LearnerDetails learner={learner} />}
-            </li>
-          )
-        }
-      </ul>
-    </main>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/add-learner">Add Learner</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/add-learner" exact>
+          <AddLearner />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import 'firebase/database';
+import 'firebase/firestore';
+import { FirebaseAppProvider } from 'reactfire'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
